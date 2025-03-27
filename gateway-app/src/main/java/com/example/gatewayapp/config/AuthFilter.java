7a06844f3a6d30a4c31caf656ceb94ff76225c6c
chuchudz999 <chuchudz999@gmail.com>
2025-03-27 12:06:00 +0700

    gateway app for user

---
+++ gateway-app/src/main/java/com/example/gatewayapp/config/AuthFilter.java
@@ -0,0 +1,62 @@
+package com.example.gatewayapp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public AuthFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            String path = exchange.getRequest().getURI().getPath();
+            if (path.startsWith("/api/register") || 
+                path.startsWith("/api/auth/login") || 
+                path.startsWith("/api/auth/forgot-password") || 
+                path.startsWith("/api/auth/verify-code") || 
+                path.startsWith("/api/auth/reset-password")) {
+                return chain.filter(exchange); // Bỏ qua xác thực
+            }
+
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                String token = authHeader.substring(7);
+                try {
+                    String role = restTemplate.getForObject("http://localhost:8081/api/auth/validate?token=" + token, String.class);
+                    if (role == null) {
+                        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                        return exchange.getResponse().setComplete();
+                    }
+                } catch (Exception e) {
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete();
+                }
+            } else {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+
+    public static class Config {
+    }
+}
