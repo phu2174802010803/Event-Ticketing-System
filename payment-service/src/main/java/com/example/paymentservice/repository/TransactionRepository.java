644084a499af5d88697e04c03db9d549d8ea7758
chuchudz999 <chuchudz999@gmail.com>
2025-05-28 10:30:43 +0700

    Change transaction ID type in TransactionRepository from Integer to String

--- payment-service/src/main/java/com/example/paymentservice/repository/TransactionRepository.java
+++ payment-service/src/main/java/com/example/paymentservice/repository/TransactionRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+public interface TransactionRepository extends JpaRepository<Transaction, String> {
     Optional<Transaction> findByTransactionId(String transactionId);
 }
